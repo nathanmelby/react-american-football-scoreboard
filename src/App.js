ed4284dc5247c88fea8ed3c15d91d425f45ed0ba
Nathan Melby <nathan@Nathans-Mac-mini.local>
2019-09-09 16:51:09 -0500

    attempted timer+increase quarter
    Co-authored-by: Meera Andersen <38503526+meera-andersen@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@
 import React, { useState }  from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
-​
+
 function App() {
   //TODO: STEP 2 - Establish your application's state with some useState hooks.  
   //You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(32);
   const [awayScore, setAwayScore] = useState(32);
+  const [seconds, setTimer] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+
+  // const [currentQuarter, setQuarter] = useState(1);
+ 
+
   // const[timer, setTimer] = useState(3)
   return (
     <div className="container">
@@ -21,7 +28,7 @@ function App() {
 ​
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{seconds}s</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
@@ -39,12 +46,17 @@ function App() {
           <button className="awayButtons__touchdown" onClick = {() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick = {() => setAwayScore(awayScore +3)}>Away Field Goal</button>
         </div>
+
+        {/* <div className="timerButtons">
+          <button className="timerButton" onClick={toggle}>
+    {isActive ? 'Pause' : 'Start'}} </button>
+        </div> */}
+
+
       </section>
     </div>
   );
 }
-​
-​
-​
-​
+
+
 export default App;
